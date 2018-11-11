8d6d5e7cfb0c2b501199a39810e0e75d7302a544
BruceZhou123 <2731760609@qq.com>
2018-11-11 12:20:39 +0800

    set global .gitignore

--- git_set.sh
+++ git_set.sh
@@ -2,3 +2,7 @@
 
 git config --global user.name "BruceZhou123"
 git config --global user.email 2731760609@qq.com
+git config --global core.excludesfile ~/.gitignore
+cd ~/
+wget https://github.com/github/gitignore/blob/master/Python.gitignore
+mv Python.gitignore .gitignore
